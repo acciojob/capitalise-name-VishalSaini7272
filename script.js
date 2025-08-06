6d32b2703e775d04df5eccbd5605c81cb7540b23
VishalSaini7272 <sainivishal156@gmail.com>
2025-08-06 12:25:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 window.addEventListener("DOMContentLoaded", function () {
 let input = document.getElementById("fname")
-input.addEventListner ("blur", function(){
+input.addEventListener ("blur", function(){
 	input.value = input.value.toUpperCase();
 });
 });
